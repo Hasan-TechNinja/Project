b7eca5482ea0cc6008e27dcc0645fe7e250cf7fc
Hasan-TechNinja <hasantechninja@gmail.comm>
2024-09-18 13:39:33 +0600

    profile design update

--- Profile/views.py
+++ Profile/views.py
@@ -1,15 +1,3 @@
-# from django.shortcuts import render
-# from django.contrib.auth.models import User
-
-# # Create your views here.
-
-
-# def Profile(request):
-
-
-#     return render(request, 'profile.html')
-
-
 from django.shortcuts import render, get_object_or_404
 from .models import ProfileModel
 
@@ -17,8 +5,8 @@ def Profile(request):
     # Get the profile for the logged-in user
     profile = get_object_or_404(ProfileModel, user=request.user)
     
-    # Pass the profile to the template
+
     context = {
         'profile': profile
     }
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles.html', context)
