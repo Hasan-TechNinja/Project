a5df8d7988636bd3b35197ead48ac49a1182197b
Hasan-TechNinja <hasantechninja@gmail.comm>
2024-09-30 15:43:16 +0600

    update profile design and show cart product on profile page

--- Profile/views.py
+++ Profile/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import ProfileModel
+from Main.models import Cart
 from django.shortcuts import redirect
 from .forms import ProfileForm
 
 @login_required(login_url='login')
 def Profile(request):
-    profile = get_object_or_404(ProfileModel, user=request.user)
+    user = request.user
+    profile = get_object_or_404(ProfileModel, user = user)
+    cart = Cart.objects.filter(user = user).order_by('-id')
     name = 'Your Name'
     if profile.first_name:
         name = str(profile.first_name + ' ' + profile.last_name)
 
     context = {
         'profile': profile,
-        'name': name
+        'name': name,
+        'cart': cart
     }
     return render(request, 'profile.html', context)
 
